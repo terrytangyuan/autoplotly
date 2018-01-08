b9365e8c9c6ddc828c0cff37c76d27644fb47ac3
terrytangyuan <terrytangyuan@gmail.com>
2018-01-08 17:46:29 -0500

    backup but may not work

--- R/package.R
+++ R/package.R
@@ -2,7 +2,7 @@
 #' 
 #' This package provides functionalities to automatically generate interactive visualizations for 
 #' many popular statistical results supported by ggfortify package with `plotly.js` and `ggplot2` style. 
-#' The generated plot can also be easily extended using `ggplot2` syntax while staying interactive.
+#' The generated plot can also be easily extended using `ggplot2` and `plotly` syntax while staying interactive.
 #' 
 #' @docType package
 #' @name autoplotly
