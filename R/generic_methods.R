93e6c21173f10ca06240c3d0e57d50a1221da739
terrytangyuan <terrytangyuan@gmail.com>
2018-01-07 22:01:52 -0500

    Docs

--- R/generic_methods.R
+++ R/generic_methods.R
@@ -13,12 +13,27 @@
   autoplotly(ggplot_obj)
 }
 
-#' @title Automatic Visualization of Popular Statistical Results Using plotly and ggplot2
+#' Automatic Visualization of Popular Statistical Results Using plotly and ggplot2
+#'
 #' @description This function provides functionality to automatically generate interactive
 #' plot for many popular statistical results supported by `ggfortify` package using plotly and ggplot2.
 #'
-#' @inheritParams autoplotly.default
+#' @param object The object that represents your statistical result, e.g. `stats::prcomp(iris[-5])`.
+#' @param ... Arguments passed to ggfortify's autoplot function for the applied
+#'   object. For example, if your object is constructed from `stats::prcomp(iris[-5])`,
+#'   you can find the documentation for the list of additional arguments via
+#'   `?ggfortify:::autoplot.prcomp()`.
+#' @inheritParams plotly::ggplotly
+#'
 #' @export
+#' 
+#' @examples
+#' # Automatically generate interactive plot for results produced by `stats::prcomp`
+#' p <- autoplotly(prcomp(iris[c(1, 2, 3, 4)]), data = iris, colour = 'Species', label = TRUE, label.size = 3, frame = TRUE)
+#' 
+#' # You can apply additional ggplot2 elements to interactive plot built using `autoplotly()`
+#' p <- p + ggplot2::ggtitle("This is a custom title") + ggplot2::labs(y = "Second Principal Components", x = "First Principal Components")
+#' p
 autoplotly <- function(object,
                        ...,
                        width = NULL,
